560fb299c1be41edda0a4841cdf386d8f856e8aa
yafiet kidane <yafietkidane@student-235-4.eduroam.uu.se>
2025-02-24 14:42:47 +0100

    a working player hand implementation, but need to handle representation of dupelicate cards, we have 2 of each

--- testing.ts
+++ testing.ts
@@ -1,20 +1,48 @@
 //hello
+// UNO cards we need to have
 
+//red, green, blue, yellow: 2 * (1-9, skip, reverse, +2) 
+//  one 0 in each color
+// 4 *  wild only pick color
+// 4 * wild pick color and draw 4
+//tot 108 cards:
 
 type Color = "red" | "yellow" | "blue" | "green" | "wild";
 
 type Value = number | "+4" | "+2" | "new color" | "skip" | "reverse";
 
+// ex: {color: "red", value: 4}
+type Card_info = {color: Color, value: Value}; 
 
-type card = {color: Color, value: Value}
+// ex {red4: {color: "red", value: 4}}
+type Card = {[tag: string]: Card_info};
 
+//ex {red4: {color: "red", value: 4}, blue6: { color: 'blue', value: 6 }}
 
+type Hand = Record<string, Card_info>;
 
+//removing card: by its tag/key
 
-// const foo: card = {color: "red", value: 7};
+//ex delete player_hand[key];
 
-// foo.color = "green";
-// foo.value = "new color";
+//adding card to hand: needs tag and card_info:
+// player_hand[new_key]
 
-// console.log(foo);
+//making a hand, that is easy to pick from, and 
+
+const player_hand: Hand = {};
+
+player_hand["red4"] = {color: "red", value: 4};
+
+//console.log(player_hand);
+
+player_hand["blue6"] = {color: "blue", value: 6};
+player_hand["draw4"] = {color: "wild", value: "+4"};
+player_hand["green9"] = {color: "green", value: 9};
+
+console.log("color should be wild ==== ", player_hand.draw4.color);
+console.log("after adding: ");
+
+//gives number of keys in the record:
+console.log(Object.keys(player_hand).length);
 
