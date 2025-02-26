19abc63450ba2928490336080794c6dc1583a03b
Yafiet Kidane <yafieta50@gmail.com>
2025-02-26 13:21:28 +0100

    working add card and delete card to/from hand

--- testing.ts
+++ testing.ts
@@ -2,51 +2,6 @@ import { is_null, head, list, tail, List, pair, append } from "./lib/list";
 import { Card, Color, Value, Card_info, Hand } from "./types";
 import { Queue, empty as empty_q, is_empty as is_empty_q, enqueue, dequeue, head as q_head, display_queue } from "./lib/queue_array";
 
-//red, green, blue, yellow: 2 * (1-9, skip, reverse, +2) 
-//  one 0 in each color
-// 4 *  wild only pick color
-// 4 * wild pick color and draw 4
-//tot 108 cards:
-
-//making a hand, that is easy to pick from, a record with tags as keys and card_info as value
-// const player_hand: Hand = {};
-
-// player_hand["red4"] = list({color: "red", value: 4}, {color: "red", value: 4});
-
-// //console.log(player_hand);
-
-// player_hand["blue6"] = list({color: "blue", value: 6});
-// player_hand["draw4"] = list({color: "wild", value: "+4"});
-// player_hand["green9"] = list({color: "green", value: 9});
-
-// const test1 = player_hand.red4;
-
-// if(!is_null(test1)) {
-//     console.log((test1));
-//     console.log(head(test1));
-// } else {}
-
-
-
-// console.log("color should be wild ==== ", player_hand.draw4.color);
-// console.log("after adding: ");
-
-//gives number of keys in the record:
-
-//console.log(Object.keys(player_hand).length);
-
-//maybe type Card = {[tag: string]: Card_info};
-//should have list(Card_info, Card_info) and when a card is picked we only have list(Card_info), 
-//and when the last card is picked => list() then remove that key-val from the record. 
-
-//
-
-// type Card = {tag: "blue6", color: "blue", value: 6};
-
-// ex red and 
-// function make_tag(col: Color, val: Value): string {
-//     return `${col}${val}`;
-// }
 
 //example: make_card(red, 7) ===>>>> {red7: {color: red, value: 7}};
 function make_card(col: Color, val: Value): Card {
@@ -54,48 +9,32 @@ function make_card(col: Color, val: Value): Card {
         tag: `${col}${val}`,
         CI: {color: col, value: val}
     };
-    
-    //return {[`${col}${val}`]: {color: col, value: val}};
 }
-function many_enques<T>(n: number, q: Queue<T>, e: T): Queue<T> {
 
+function many_enques<T>(n: number, q: Queue<T>, e: T): Queue<T> {
+//easy to enque many times
     for(let j = 0; j < n; j++) {
         enqueue(e, q);
     }
 
     return q;
 }
-
-//display_queue(many_enques<boolean>(2, empty_q<boolean>(), true));
-
-
-//console.log(make_card("red", 9));
-
+//makes all cards from one color
 function make_color(col: Color, q: Queue<Card> ): Queue<Card> {
     const p2 = "+2";
     const skip = "skip";
     const rev = "reverse";
-    //let res = null;
-
     for(let i = 1; i < 10; i++) {
-        
         const curr = make_card(col, i); 
         many_enques(2, q, curr);
-        
     }
-
     const p2_card = make_card(col, p2);
     many_enques(2, q, p2_card);
-
     const skip_card = make_card(col, skip);
     many_enques(2, q, skip_card);
-
     const rev_card = make_card(col, rev);
     many_enques(2, q, rev_card);
-
-    //add one 0 card to q
     enqueue(make_card(col, 0), q);
-    
     return q;
 }
 
@@ -107,112 +46,57 @@ function make_wild_card(q: Queue<Card>): Queue<Card> {
     const made_pick_c = make_card(col, pick_col);
     many_enques(4, q, made_4);
     many_enques(4, q, made_pick_c);
-
     return q;
-    
 }
-
-// function make_all_cards(): Queue<Card> {
-//     const deck: Queue<Card> = empty_q();
-// function make_all_cards(): Queue<Card> {
-//     const deck: Queue<Card> = empty_q();
-
-
-// }
-
-
-//const test_deck: Queue<Card> = empty_q<Card>();
-
-//make_color("red", test_deck);
-// make_color("yellow", p_hand);
-// make_color("blue", p_hand);
-// make_color("green", p_hand);
-// make_wild_card(p_hand);
-
-// display_queue(p_hand);
-
-//now we have a deck.
-
-
-//add card to hand function test
-
-//export type Card = {[tag: string]: Card_info};
-
-//const foo = make_card("blue", 5);
-
-//console.log(foo.CI);
-    
-// }
-
-//fisher yates shuffle:
-
-
-
 function random_int(max: number): number {
     //returns number 0-max in a random fashion
     return Math.floor(Math.random() * max);
 }
-
 function swap<T>(A: Array<T>, i: number, j: number): void {
+    //swaps place on 2 indexes in an array
     const temp = A[i];
     A[i] = A[j];
     A[j] = temp;
 }
-
-
-//takes in Queue and returns queue, modifying the array in the Q:
 //fisher yates algo
 function shuffle(q: Queue<Card>): Queue<Card>  {
-    
     const q_arr = q[2];
     const len = q_arr.length;
-    
-    for(let i = len - 1; i >= 0; i--) {
-        
+    for(let i = len - 1; i >= 0; i--) { 
         let j = random_int(i);
         swap(q_arr, i, j);
     }
-    
     q[2] = q_arr;//modded Q array.
     return q;
 }
-//shuffle works
-//console.log(shuffle(test_deck)[2]);
-
-
 //function delete card from hand:
-//
-
+//OBS we have dynamic keys so for accessing values in a record we need
+// to use record[tag] instead of record.tag
 function delete_card_from_hand(c: Card, hand: Hand): boolean {
     const t = c.tag;
-
-    if(!is_null(hand.t)) {
-        hand[t] = tail(hand.t);
-
-        if(is_null(tail(hand.t))) {
+    if(!is_null(hand[t])) {
+        hand[t] = tail(hand[t]);
+        if(is_null(hand[t])) {
             delete hand[t];
         } else {}
-
+        
         return true;
     } else {
         return false;
     }
 }
-
 function add_card_to_hand(c: Card, hand: Hand): void {
     const tag = c.tag;
-
-    if(hand.tag === undefined) {
+    if(hand[tag] === undefined) {
         hand[tag] = list(c.CI);
     } else {
-        hand[tag] = append(list(c.CI), hand[tag]);
+        hand[tag] = pair(c.CI, hand[tag]);
     }
-
 }
-
 const hnd: Hand = {};
-
 add_card_to_hand(make_card("red", 4), hnd);
 add_card_to_hand(make_card("red", 4), hnd);
+console.log(hnd.red4);
+delete_card_from_hand(make_card("red", 4), hnd);
+console.log(hnd.red4);
 
-console.log(hnd);
